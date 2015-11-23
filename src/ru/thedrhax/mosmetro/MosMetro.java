1304e8eb76460999c9480de65e8fa3d14ac98604
Дмитрий Карих <the.dr.hax@gmail.com>
2015-11-23 23:31:23 +0300

    Redirect stdout to messages TextView and request current IP address after button press

--- src/ru/thedrhax/mosmetro/MosMetro.java
+++ src/ru/thedrhax/mosmetro/MosMetro.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.*;
+
+import ru.thedrhax.httpclient.*;
+
 public class MosMetro extends Activity
 {
     /** Called when the activity is first created. */
@@ -14,12 +18,22 @@ public class MosMetro extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Redirect stdout to TextView "messages"
+		final TextView messages = (TextView)findViewById(R.id.messages);
+		System.setOut(new PrintStream(new OutputStream() {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+			@Override public void write(int oneByte) throws IOException {
+				outputStream.write(oneByte);
+				messages.setText(new String(outputStream.toByteArray()));
+			}
+		}));
     }
     
     // Handling connection button
     public void connect (View view) {
-    	TextView messages = (TextView)findViewById(R.id.messages);
-    	
-    	messages.append("OK\n");
+    	HttpRequest request = new HttpRequest("http://curlmyip.org").connect();
+    	System.out.println("Your IP: " + request.getContent() + "\n");
     }
 }
