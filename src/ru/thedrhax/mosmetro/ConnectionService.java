1787e17c765d43643170723c649509b474e0a82a
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-14 08:20:41 +0300

    Add ConnectionService launched by NetworkReceiver

---
+++ src/ru/thedrhax/mosmetro/ConnectionService.java
@@ -0,0 +1,45 @@
+package ru.thedrhax.mosmetro;
+
+import android.app.IntentService;
+import android.content.Intent;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import android.os.Environment;
+
+public class ConnectionService extends IntentService {
+	private static FileWriter log_writer;
+	
+	public ConnectionService () {
+		super("ConnectionService");
+	}
+	
+	public void onCreate() {
+		super.onCreate();
+		
+		if (log_writer == null) {
+            // Open log file
+            try {
+                File log_file = new File(
+                        Environment.getExternalStorageDirectory().getPath() + "/MosMetro_Log.txt"
+                );
+                if (!log_file.exists()) log_file.createNewFile();
+
+                log_writer = new FileWriter(log_file, true);
+            } catch (IOException ignored) {}
+        }
+    }
+
+    private static final MosMetroConnection connection = new MosMetroConnection() {
+        public void log(String message) {
+            try {
+                log_writer.write(message + "\n");
+                log_writer.flush();
+            } catch (IOException ignored) {}
+        }
+	};
+	
+	public void onHandleIntent(Intent intent) {
+		connection.connect();
+	}
+}
--- src/ru/thedrhax/mosmetro/NetworkReceiver.java
+++ src/ru/thedrhax/mosmetro/NetworkReceiver.java
@@ -13,29 +13,9 @@ import java.io.IOException;
 
 public class NetworkReceiver extends BroadcastReceiver {
     private static boolean lock = false;
-    private static FileWriter log_writer;
 
     public NetworkReceiver() {
-        if (log_writer == null) {
-            // Open log file
-            try {
-                File log_file = new File(
-                        Environment.getExternalStorageDirectory().getPath() + "/MosMetro_Log.txt"
-                );
-                if (!log_file.exists()) log_file.createNewFile();
-
-                log_writer = new FileWriter(log_file, true);
-            } catch (IOException ignored) {}
-        }
-    }
-
-    private static final MosMetroConnection connection = new MosMetroConnection() {
-        public void log(String message) {
-            try {
-                log_writer.write(message + "\n");
-                log_writer.flush();
-            } catch (IOException ignored) {}
-        }
+        
     };
 
     public void onReceive(Context context, Intent intent) {
@@ -48,7 +28,7 @@ public class NetworkReceiver extends BroadcastReceiver {
 
         if (!lock && "\"MosMetro_Free\"".equals(info.getSSID())) {
             lock = true;
-            connection.connect();
+            context.startService(new Intent(context, ConnectionService.class));
         }
     }
 }
