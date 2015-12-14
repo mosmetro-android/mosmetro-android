040c9843d153d3d88a9e70a41fbf7a4cb897d1fc
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-14 14:20:35 +0300

    Move manual connection to a separated activity

--- src/ru/thedrhax/mosmetro/MosMetro.java
+++ src/ru/thedrhax/mosmetro/MosMetro.java
@@ -1,61 +1,21 @@
 package ru.thedrhax.mosmetro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
-import android.widget.TextView;
 
 public class MosMetro extends Activity {
-	// UI Elements
-	private TextView text_messages;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		text_messages = (TextView)findViewById(R.id.text_messages);
 	}
 
-	// Push received messages to the UI thread
-	private final Handler handler = new Handler() {
-		public void handleMessage(Message message) {
-			String text = message.getData().getString("text");
-			if (text == null) return;
-
-			text_messages.append(text);
-		}
-	};
-
-	// Connection sequence
-	final MosMetroConnection connection = new MosMetroConnection() {
-		// Send log messages to Handler
-		public void log (String message) {
-			Message msg = handler.obtainMessage();
-			Bundle bundle = new Bundle();
-			bundle.putString("text", message + "\n");
-			msg.setData(bundle);
-			handler.sendMessage(msg);
-		}
-	};
-
-	// Run connection sequence in background thread
-	private Thread thread;
-	private final Runnable task = new Runnable() {
-		public void run () {
-			connection.connect();
-		}
-	};
-
-	// Handle connection button
-	public void connect (View view) {
-		// Start connection thread if not already running
-		if ((thread == null) || (!thread.isAlive())) {
-			thread = new Thread(task);
-			thread.start();
-		}
-	}
+    public void button_debug (View view) {
+        Intent debug = new Intent(view.getContext(), DebugActivity.class);
+        startActivity(debug);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
 }
