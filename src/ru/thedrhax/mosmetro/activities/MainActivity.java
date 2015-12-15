07ddd189ffadd67a583e4dfccf0039cddf8c21d2
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-15 13:58:28 +0300

    Show last success time in the MainActivity

--- src/ru/thedrhax/mosmetro/activities/MainActivity.java
+++ src/ru/thedrhax/mosmetro/activities/MainActivity.java
@@ -2,9 +2,13 @@ package ru.thedrhax.mosmetro.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import ru.thedrhax.mosmetro.R;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -12,6 +16,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+        SharedPreferences settings = getSharedPreferences("MosMetro_Lock", 0);
+        Long time = Calendar.getInstance().getTimeInMillis();
+        Long lastSuccess = settings.getLong("LastSuccess", 0);
+        TextView text_last_success = (TextView)findViewById(R.id.text_last_success);
+        if (lastSuccess != 0)
+            text_last_success.setText(
+                    "Успешное подключение: " +
+                    Long.toString(((time-lastSuccess)/1000)) +
+                    " секунд назад"
+            );
 	}
 
     public void button_debug (View view) {
