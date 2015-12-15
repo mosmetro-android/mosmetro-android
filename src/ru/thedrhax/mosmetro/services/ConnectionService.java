5fc00405f0b3469f94150d561cd7edeb38cecaa1
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-15 13:16:56 +0300

    Store last success timestamp in SharedPreferences instead of static field

--- src/ru/thedrhax/mosmetro/services/ConnectionService.java
+++ src/ru/thedrhax/mosmetro/services/ConnectionService.java
@@ -2,6 +2,7 @@ package ru.thedrhax.mosmetro.services;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Environment;
 import ru.thedrhax.mosmetro.MosMetroConnection;
 
@@ -12,7 +13,6 @@ import java.util.Calendar;
 
 public class ConnectionService extends IntentService {
 	private static FileWriter log_writer;
-	private static long lastSuccess = 0;
 	
 	public ConnectionService () {
 		super("ConnectionService");
@@ -44,8 +44,19 @@ public class ConnectionService extends IntentService {
 	};
 	
 	public void onHandleIntent(Intent intent) {
-		if (Calendar.getInstance().getTimeInMillis() < lastSuccess + 5*60*1000) return;
+        SharedPreferences settings = getSharedPreferences("MosMetro_Lock", 0);
 
-		if (connection.connect()) lastSuccess = Calendar.getInstance().getTimeInMillis();
+        Long time = Calendar.getInstance().getTimeInMillis();
+        Long lastSuccess = settings.getLong("LastSuccess", 0);
+
+        if (time < lastSuccess + 5*60*1000) return;
+
+        SharedPreferences.Editor editor = settings.edit();
+
+		if (connection.connect()) {
+            time = Calendar.getInstance().getTimeInMillis();
+            editor.putLong("LastSuccess", time);
+            editor.apply();
+        }
 	}
 }
