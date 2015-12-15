2564941a4ade210d4721e999db16b7a53e796dab
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-15 22:18:09 +0300

    Remove logging to file

--- src/ru/thedrhax/mosmetro/services/ConnectionService.java
+++ src/ru/thedrhax/mosmetro/services/ConnectionService.java
@@ -3,45 +3,20 @@ package ru.thedrhax.mosmetro.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Environment;
 import ru.thedrhax.mosmetro.MosMetroConnection;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Calendar;
 
 public class ConnectionService extends IntentService {
-	private static FileWriter log_writer;
-	
 	public ConnectionService () {
 		super("ConnectionService");
 	}
 	
 	public void onCreate() {
 		super.onCreate();
-
-		if (log_writer == null) {
-            // Open log file
-            try {
-                File log_file = new File(
-                        Environment.getExternalStorageDirectory().getPath() + "/MosMetro_Log.txt"
-                );
-                if (!log_file.exists()) log_file.createNewFile();
-
-                log_writer = new FileWriter(log_file, true);
-            } catch (IOException ignored) {}
-        }
     }
 
-    private static final MosMetroConnection connection = new MosMetroConnection() {
-        public void log(String message) {
-            try {
-                log_writer.write(message + "\n");
-                log_writer.flush();
-            } catch (IOException ignored) {}
-        }
-	};
+    private static final MosMetroConnection connection = new MosMetroConnection();
 	
 	public void onHandleIntent(Intent intent) {
         SharedPreferences settings = getSharedPreferences("MosMetro_Lock", 0);
