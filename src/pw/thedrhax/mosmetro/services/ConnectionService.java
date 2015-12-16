0d33b2f8d12773201a7660e9bed2f58a56a0062c
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-17 00:31:12 +0300

    Optimize ConnectionService

--- src/pw/thedrhax/mosmetro/services/ConnectionService.java
+++ src/pw/thedrhax/mosmetro/services/ConnectionService.java
@@ -8,25 +8,27 @@ import pw.thedrhax.mosmetro.MosMetroConnection;
 import java.util.Calendar;
 
 public class ConnectionService extends IntentService {
+    private SharedPreferences lock;
+    private SharedPreferences.Editor editor;
+
 	public ConnectionService () {
 		super("ConnectionService");
 	}
 	
 	public void onCreate() {
 		super.onCreate();
+
+        lock = getSharedPreferences("MosMetro_Lock", 0);
+        editor = lock.edit();
     }
 
     private static final MosMetroConnection connection = new MosMetroConnection();
 	
 	public void onHandleIntent(Intent intent) {
-        SharedPreferences settings = getSharedPreferences("MosMetro_Lock", 0);
-
         Long time = Calendar.getInstance().getTimeInMillis();
-        Long lastSuccess = settings.getLong("LastSuccess", 0);
+        Long lastSuccess = lock.getLong("LastSuccess", 0);
 
         if (time < lastSuccess + 60*1000) return;
-
-        SharedPreferences.Editor editor = settings.edit();
 
 		if (connection.connect()) {
             time = Calendar.getInstance().getTimeInMillis();
