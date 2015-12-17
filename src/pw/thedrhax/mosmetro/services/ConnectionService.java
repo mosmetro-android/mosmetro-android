9b0c11d39141b5d2beab694653d789d422aed0bd
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-17 12:08:51 +0300

    Remove last success count from MainLayout

--- src/pw/thedrhax/mosmetro/services/ConnectionService.java
+++ src/pw/thedrhax/mosmetro/services/ConnectionService.java
@@ -7,11 +7,8 @@ import android.preference.PreferenceManager;
 import pw.thedrhax.mosmetro.MosMetroConnection;
 import pw.thedrhax.util.Util;
 
-import java.util.Calendar;
-
 public class ConnectionService extends IntentService {
     private SharedPreferences settings;
-    private SharedPreferences lock;
 
     public ConnectionService () {
 		super("ConnectionService");
@@ -21,26 +18,12 @@ public class ConnectionService extends IntentService {
 		super.onCreate();
 
         settings = PreferenceManager.getDefaultSharedPreferences(this);
-        lock = getSharedPreferences("MosMetro_Lock", 0);
     }
 
     private static final MosMetroConnection connection = new MosMetroConnection();
 	
 	public void onHandleIntent(Intent intent) {
-        Long time = Calendar.getInstance().getTimeInMillis();
-        Long lastSuccess = lock.getLong("LastSuccess", 0);
-
-        if (time < lastSuccess + 60*1000) return;
-
         int result = connection.connect();
-
-		if (result < 2) {
-            time = Calendar.getInstance().getTimeInMillis();
-
-            SharedPreferences.Editor editor = lock.edit();
-            editor.putLong("LastSuccess", time);
-            editor.apply();
-        }
 
         if ((result == 0) && (settings.getBoolean("pref_notify", true)))
             Util.notify(this,
