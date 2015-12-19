2a98b24572097a083167a44d57df03ece2386068
Дмитрий Карих <the.dr.hax@gmail.com>
2015-12-19 21:05:24 +0300

    Add app and developer info to SettingsActivity

--- src/pw/thedrhax/mosmetro/activities/SettingsActivity.java
+++ src/pw/thedrhax/mosmetro/activities/SettingsActivity.java
@@ -1,10 +1,11 @@
 package pw.thedrhax.mosmetro.activities;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import pw.thedrhax.mosmetro.R;
 
@@ -24,10 +25,22 @@ public class SettingsActivity extends Activity {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         } catch (NullPointerException ignored) {}
 
+        // Populate preferences
+        SettingsFragment settings = new SettingsFragment();
         getFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(android.R.id.content, settings)
                 .commit();
+        getFragmentManager().executePendingTransactions();
+
+        // Add version name and code
+        Preference app_name = settings.findPreference("app_name");
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            app_name.setSummary("Версия: " + pInfo.versionName + "-" + pInfo.versionCode);
+        } catch (PackageManager.NameNotFoundException ex) {
+            app_name.setSummary("");
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
