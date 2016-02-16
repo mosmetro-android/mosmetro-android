08aaca699af4d76a2399e804dcd9781d84575138
Дмитрий Карих <the.dr.hax@gmail.com>
2016-02-16 22:57:18 +0300

    NetworkReceiver: Try to reconnect to the network if disconnected (fixes #7)
    New captive portal installed on the network makes Android disconnect from Wi-Fi after 1-2 minutes and it doesn't try to connect again. This option will force reconnect the device to the network immediately.
    
    (cherry picked from commit 9b14059)

--- src/pw/thedrhax/mosmetro/services/NetworkReceiver.java
+++ src/pw/thedrhax/mosmetro/services/NetworkReceiver.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.preference.PreferenceManager;
 import pw.thedrhax.util.Notification;
 
+import java.util.List;
+
 public class NetworkReceiver extends BroadcastReceiver {
+    private static final String NETWORK_SSID = "\"MosMetro_Free\"";
+
     public void onReceive(Context context, Intent intent) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
-
         SharedPreferences.Editor settings_editor = settings.edit();
 
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -23,16 +27,30 @@ public class NetworkReceiver extends BroadcastReceiver {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 
-        if (networkInfo != null &&
-                networkInfo.isConnected() &&
-                "\"MosMetro_Free\"".equals(wifiInfo.getSSID())) {
+        if (networkInfo != null && // network info exists
+            networkInfo.isConnected() && // network is connected
+            NETWORK_SSID.equals(wifiInfo.getSSID())) { // wi-fi SSID is "MosMetro_Free"
 
+            // if not locked, then lock and start the ConnectionService
             if (!settings.getBoolean("locked", false) && settings.getBoolean("pref_autoconnect", true)) {
                 settings_editor.putBoolean("locked", true);
                 settings_editor.apply();
                 context.startService(new Intent(context, ConnectionService.class));
             }
         } else {
+            // Try to reconnect the Wi-Fi network
+            if (settings.getBoolean("pref_wifi_reconnect", false) &&
+                settings.getBoolean("locked", false)) {
+
+                List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+                for (WifiConfiguration network : list) {
+                    if (network.SSID.equals(NETWORK_SSID)) {
+                        wifiManager.enableNetwork(network.networkId, true);
+                        wifiManager.reconnect();
+                    }
+                }
+            }
+
             settings_editor.putBoolean("locked", false);
             settings_editor.apply();
             new Notification(context).setId(0).hide(); // Hide result notification on disconnect
