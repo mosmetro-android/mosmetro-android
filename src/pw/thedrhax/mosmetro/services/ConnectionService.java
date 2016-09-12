mosmetro-android/mosmetro-android

commit 64079e54667a9fd451b2266ddb14a45507a017b0

ConnectionService: Get SSID from Intent extras on API > 14 (fixes #52)
This commit also fixes #46 because it contains the full rewrite of working with SSID's. And it *may* fix an issue #41.

src/pw/thedrhax/mosmetro/services/ConnectionService.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -21,7 +20,10 @@ import pw.thedrhax.util.Logger;
 import pw.thedrhax.util.Notification;
 
 public class ConnectionService extends IntentService {
+    private static final String UNKNOWN_SSID = "<unknown ssid>";
+
     private static boolean running = false;
+    private static String SSID = UNKNOWN_SSID;
     private boolean from_shortcut = false;
 
     // Preferences
@@ -134,32 +136,7 @@ public class ConnectionService extends IntentService {
     }
 
     private boolean isWifiConnected() {
-        if (from_shortcut) return true;
-        
-        // Check if Wi-Fi is not enabled
-        if (!manager.isWifiEnabled())
-            return false;
-
-        WifiInfo info = manager.getConnectionInfo();
-
-        // Strict check by supplicant state
-        if (settings.getBoolean("pref_autoconnect_strict", false))
-            if (!info.getSupplicantState().equals(SupplicantState.COMPLETED))
-                return false;
-
-        if (connection == null) {
-            for (Class<? extends Authenticator> authenticator : Authenticator.SUPPORTED_NETWORKS) {
-                try {
-                    if (info.getSSID().replace("\"", "").equals(authenticator.getField("SSID").get(authenticator)))
-                        return true;
-                } catch (Exception ignored) {}
-            }
-        } else {
-            if (connection.getSSID().equals(info.getSSID().replace("\"", "")))
-                return true;
-        }
-
-        return false;
+        return connection.getSSID().equals(SSID) || from_shortcut;
     }
 
     private boolean waitForIP() {
@@ -254,17 +231,23 @@ public class ConnectionService extends IntentService {
             if (result == Authenticator.STATUS_NOT_REGISTERED) break;
         } while (++count < pref_retry_count && result > Authenticator.STATUS_ALREADY_CONNECTED && running);
 
-        // Remove progress notification
-        notify_progress.hide();
-
         return result;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        WifiInfo info;
+
+        if (Build.VERSION.SDK_INT >= 14) {
+            info = intent.getParcelableExtra(WifiManager.EXTRA_WIFI_INFO);
+        } else {
+            info = manager.getConnectionInfo();
+        }
+        SSID = info != null ? info.getSSID().replace("\"", "") : UNKNOWN_SSID;
+
         if ("STOP".equals(intent.getAction())) { // Stop by intent
             stopSelf();
-        } else if (!running) { // Start if not already running
+        } else if (!(UNKNOWN_SSID.equals(SSID) || running)) {
             onStart(intent, startId);
         }
         return START_NOT_STICKY;
@@ -276,7 +259,7 @@ public class ConnectionService extends IntentService {
         running = false;
     }
     
-    public void main(Intent intent) {
+    private void main(Intent intent) {
         // Check if started from one of the shortcuts
         if (intent.getStringExtra("SSID") != null) {
             pref_notify_success_lock = false;
@@ -285,13 +268,11 @@ public class ConnectionService extends IntentService {
 
         logger.date();
 
-        // Check if Wi-Fi is connected
-        if (!isWifiConnected()) return;
-
+        // Select an Authenticator
         if (from_shortcut) {
             connection = new Chooser(this, true, logger).choose(intent.getStringExtra("SSID"));
         } else {
-            connection = new Chooser(this, true, logger).choose();
+            connection = new Chooser(this, true, logger).choose(SSID);
         }
         if (connection == null) return;
 
@@ -316,6 +297,7 @@ public class ConnectionService extends IntentService {
 
         // Try to connect
         int result = connect();
+        notify_progress.hide();
 
         logger.date();
 

src/pw/thedrhax/mosmetro/services/NetworkReceiver.java
@@ -10,7 +10,11 @@ public class NetworkReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
 
+        Intent service = new Intent(context, ConnectionService.class);
+        service.setAction(intent.getAction());
+        service.putExtras(intent);
+
         if (settings.getBoolean("pref_autoconnect", true))
-            context.startService(new Intent(context, ConnectionService.class));
+            context.startService(service);
     }
 }

commit 5c135d3671ea5aa784db5b1895b50063e54bb3f5

ConnectionService: Fix starting from shortcut
 This commit fixes a regression introduced in 64079e54667a9fd451b2266ddb14a45507a017b0.

src/pw/thedrhax/mosmetro/services/ConnectionService.java
@@ -238,14 +238,19 @@ public class ConnectionService extends IntentService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        WifiInfo info;
-
-        if (Build.VERSION.SDK_INT >= 14) {
-            info = intent.getParcelableExtra(WifiManager.EXTRA_WIFI_INFO);
+        if (intent.getStringExtra("SSID") != null) {
+            SSID = intent.getStringExtra("SSID");
+            pref_notify_success_lock = false;
+            from_shortcut = true;
         } else {
-            info = manager.getConnectionInfo();
+            WifiInfo info;
+            if (Build.VERSION.SDK_INT >= 14) {
+                info = intent.getParcelableExtra(WifiManager.EXTRA_WIFI_INFO);
+            } else {
+                info = manager.getConnectionInfo();
+            }
+            SSID = info != null ? info.getSSID().replace("\"", "") : UNKNOWN_SSID;
         }
-        SSID = info != null ? info.getSSID().replace("\"", "") : UNKNOWN_SSID;
 
         if ("STOP".equals(intent.getAction())) { // Stop by intent
             stopSelf();
@@ -257,25 +262,15 @@ public class ConnectionService extends IntentService {
 
     public void onHandleIntent(Intent intent) {
         running = true;
-        main(intent);
+        main();
         running = false;
     }
     
-    private void main(Intent intent) {
-        // Check if started from one of the shortcuts
-        if (intent.getStringExtra("SSID") != null) {
-            pref_notify_success_lock = false;
-            from_shortcut = true;
-        }
-
+    private void main() {
         logger.date();
 
         // Select an Authenticator
-        if (from_shortcut) {
-            connection = new Chooser(this, true, logger).choose(intent.getStringExtra("SSID"));
-        } else {
-            connection = new Chooser(this, true, logger).choose(SSID);
-        }
+        connection = new Chooser(this, true, logger).choose(SSID);
         if (connection == null) return;
 
         connection.setLogger(logger);