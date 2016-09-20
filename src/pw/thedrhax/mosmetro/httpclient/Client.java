865d391344d14e7371e18917839fa76ef6a53d62
Дмитрий Карих <the.dr.hax@gmail.com>
2016-09-20 19:35:58 +0300

    Client: Use more strict check for meta-redirect
    This will avoid conflicts with MosGorTrans_Free providers in future.

--- src/pw/thedrhax/mosmetro/httpclient/Client.java
+++ src/pw/thedrhax/mosmetro/httpclient/Client.java
@@ -66,7 +66,7 @@ public abstract class Client {
         for (Element element : document.getElementsByTag("meta")) {
             if (element.attr("http-equiv").equalsIgnoreCase("refresh")) {
                 String attr = element.attr("content");
-                if (attr.contains("URL=")) {
+                if (attr.toLowerCase().contains("; url=")) {
                     link = attr.substring(attr.indexOf("=") + 1);
                 } else {
                     link = attr.substring(attr.indexOf(";") + 1);
