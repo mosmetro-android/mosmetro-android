mosmetro-android/mosmetro-android

commit 4003abef7b5ab10b7b194e170ceb8edb1a70967e

Authenticator: Use enum instead of static constants

src/pw/thedrhax/mosmetro/services/ConnectionService.java
@@ -80,12 +80,12 @@ public class ConnectionService extends IntentService {
         logger = new Logger();
     }
 
-    private void notify (int result) {
+    private void notify (Authenticator.RESULT result) {
         if (!running) return;
 
         switch (result) {
-            case Authenticator.STATUS_CONNECTED:
-            case Authenticator.STATUS_ALREADY_CONNECTED:
+            case CONNECTED:
+            case ALREADY_CONNECTED:
                 notification
                         .setTitle(getString(R.string.notification_success))
                         .setIcon(pref_colored_icons ?
@@ -111,7 +111,7 @@ public class ConnectionService extends IntentService {
 
                 return;
 
-            case Authenticator.STATUS_NOT_REGISTERED:
+            case NOT_REGISTERED:
                 notification
                         .setTitle(getString(R.string.notification_not_registered))
                         .setText(getString(R.string.notification_not_registered_register))
@@ -127,7 +127,7 @@ public class ConnectionService extends IntentService {
 
                 return;
 
-            case Authenticator.STATUS_ERROR:
+            case ERROR:
                 notification
                         .setTitle(getString(R.string.notification_error))
                         .setText(getString(R.string.notification_error_log))
@@ -190,11 +190,12 @@ public class ConnectionService extends IntentService {
         return true;
     }
 
-    private int connect() {
-        int result, count = 0;
+    private Authenticator.RESULT connect() {
+        Authenticator.RESULT result;
+        int count = 0;
 
         do {
-            if (!waitForIP()) return Authenticator.STATUS_ERROR;
+            if (!waitForIP()) return Authenticator.RESULT.ERROR;
 
             if (count > 0) {
                 notify_progress
@@ -232,11 +233,14 @@ public class ConnectionService extends IntentService {
                         getString(R.string.error),
                         getString(R.string.auth_error_network_disconnected)
                 ));
-                result = Authenticator.STATUS_ERROR; break;
+                result = Authenticator.RESULT.ERROR; break;
             }
 
-            if (result == Authenticator.STATUS_NOT_REGISTERED) break;
-        } while (++count < pref_retry_count && result > Authenticator.STATUS_ALREADY_CONNECTED && running);
+            if (result == Authenticator.RESULT.NOT_REGISTERED) break;
+        } while (++count < pref_retry_count && running &&
+                (result == Authenticator.RESULT.ALREADY_CONNECTED
+                        || result == Authenticator.RESULT.CONNECTED)
+        );
 
         return result;
     }
@@ -305,7 +309,7 @@ public class ConnectionService extends IntentService {
         } catch (InterruptedException ignored) {}
 
         // Try to connect
-        int result = connect();
+        Authenticator.RESULT result = connect();
         notify_progress.hide();
 
         logger.date();
@@ -313,7 +317,8 @@ public class ConnectionService extends IntentService {
         // Notify user if still connected to Wi-Fi
         if (isWifiConnected()) notify(result);
 
-        if (from_shortcut || result > Authenticator.STATUS_ALREADY_CONNECTED) return;
+        if (from_shortcut || (result == Authenticator.RESULT.ALREADY_CONNECTED
+                || result == Authenticator.RESULT.CONNECTED)) return;
 
         // Wait while internet connection is available
         int count = 0;
@@ -325,7 +330,7 @@ public class ConnectionService extends IntentService {
             // Check internet connection each 10 seconds
             if (settings.getBoolean("pref_internet_check", true) && ++count == 10) {
                 count = 0;
-                if (connection.isConnected() != Authenticator.CHECK_CONNECTED)
+                if (connection.isConnected() != Authenticator.CHECK.CONNECTED)
                     break;
             }
         }

src/pw/thedrhax/mosmetro/authenticator/Authenticator.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
 import pw.thedrhax.mosmetro.R;
+import pw.thedrhax.mosmetro.authenticator.networks.MosGorTrans;
 import pw.thedrhax.mosmetro.updater.URLs;
 import pw.thedrhax.mosmetro.authenticator.networks.AURA;
 import pw.thedrhax.mosmetro.authenticator.networks.MosMetro;
@@ -20,19 +21,19 @@ import java.util.Map;
 
 public abstract class Authenticator {
     public static final Class<? extends Authenticator>[] SUPPORTED_NETWORKS =
-            new Class[] {MosMetro.class, AURA.class};
+            new Class[] {MosMetro.class, AURA.class, MosGorTrans.class};
 
     // Result state
-    public static final int STATUS_CONNECTED = 0;
-    public static final int STATUS_ALREADY_CONNECTED = 1;
-    public static final int STATUS_NOT_REGISTERED = 2;
-    public static final int STATUS_ERROR = 3;
-    public static final int STATUS_INTERRUPTED = 4;
+    public static enum RESULT {
+        CONNECTED, ALREADY_CONNECTED,
+        NOT_REGISTERED, ERROR,
+        INTERRUPTED, UNSUPPORTED
+    }
 
     // Network check state
-    public static final int CHECK_CONNECTED = 0;
-    public static final int CHECK_WRONG_NETWORK = 1;
-    public static final int CHECK_NOT_CONNECTED = 2;
+    public static enum CHECK {
+        CONNECTED, WRONG_NETWORK, NOT_CONNECTED
+    }
 
 	protected Logger logger;
     protected Client client;
@@ -54,16 +55,16 @@ public abstract class Authenticator {
 
     public abstract String getSSID();
 
-    public int start() {
+    public RESULT start() {
         stopped = false;
 
         logger.log(Logger.LEVEL.DEBUG,
                    String.format(context.getString(R.string.version), getVersion()));
-        int result = connect();
+        RESULT result = connect();
 
         if (stopped) return result;
 
-        if (result <= STATUS_ALREADY_CONNECTED)
+        if (result == RESULT.CONNECTED || result == RESULT.ALREADY_CONNECTED)
             submit_info(result);
 
         return result;
@@ -85,9 +86,9 @@ public abstract class Authenticator {
      * Connection sequence
      */
 
-    public abstract int isConnected();
+    public abstract CHECK isConnected();
 
-    protected abstract int connect();
+    protected abstract RESULT connect();
 
     /*
      * Progress reporting
@@ -122,13 +123,13 @@ public abstract class Authenticator {
         }
     }
 
-    private void submit_info (int result) {
+    private void submit_info (RESULT result) {
         String STATISTICS_URL = new CachedRetriever(context)
                 .get(URLs.STAT_URL_SRC, URLs.STAT_URL_DEF) + URLs.STAT_REL_CHECK;
 
         Map<String,String> params = new HashMap<String, String>();
         params.put("version", getVersion());
-        params.put("connected", result == STATUS_CONNECTED ? "1" : "0");
+        params.put("connected", result == RESULT.CONNECTED ? "1" : "0");
         params.put("ssid", getSSID());
 
         try {

src/pw/thedrhax/mosmetro/authenticator/networks/MosMetro.java
@@ -31,20 +31,20 @@ public class MosMetro extends Authenticator {
     }
 
     @Override
-    public int connect() {
+    public RESULT connect() {
         Map<String,String> fields = null;
 
-        if (stopped) return STATUS_INTERRUPTED;
+        if (stopped) return RESULT.INTERRUPTED;
         progressListener.onProgressUpdate(0);
 
         logger.log(String.format(context.getString(R.string.auth_connecting), getSSID()));
 
         logger.log(context.getString(R.string.auth_checking_connection));
-        int connected = isConnected();
-        if (connected == CHECK_CONNECTED) {
+        CHECK connected = isConnected();
+        if (connected == CHECK.CONNECTED) {
             logger.log(context.getString(R.string.auth_already_connected));
-            return STATUS_ALREADY_CONNECTED;
-        } else if (connected == CHECK_WRONG_NETWORK) {
+            return RESULT.ALREADY_CONNECTED;
+        } else if (connected == CHECK.WRONG_NETWORK) {
             logger.log(String.format(
                     context.getString(R.string.error),
                     context.getString(R.string.auth_error_network)
@@ -57,12 +57,12 @@ public class MosMetro extends Authenticator {
                 manager.reassociate();
             }
 
-            return STATUS_ERROR;
+            return RESULT.ERROR;
         }
 
         logger.log(String.format(context.getString(R.string.auth_version), version));
 
-        if (stopped) return STATUS_INTERRUPTED;
+        if (stopped) return RESULT.INTERRUPTED;
         progressListener.onProgressUpdate(25);
 
         logger.log(context.getString(R.string.auth_auth_page));
@@ -80,7 +80,7 @@ public class MosMetro extends Authenticator {
                     context.getString(R.string.error),
                     context.getString(R.string.auth_error_auth_page)
             ));
-            return STATUS_ERROR;
+            return RESULT.ERROR;
         }
 
         if (version == 1) {
@@ -91,7 +91,7 @@ public class MosMetro extends Authenticator {
                             context.getString(R.string.error),
                             context.getString(R.string.auth_error_not_registered)
                     ));
-                    return STATUS_NOT_REGISTERED;
+                    return RESULT.NOT_REGISTERED;
                 }
                 fields = Client.parseForm(forms.first());
             } catch (Exception ex) {
@@ -99,11 +99,11 @@ public class MosMetro extends Authenticator {
                         context.getString(R.string.error),
                         context.getString(R.string.auth_error_auth_form)
                 ));
-                return STATUS_ERROR;
+                return RESULT.ERROR;
             }
         }
 
-        if (stopped) return STATUS_INTERRUPTED;
+        if (stopped) return RESULT.INTERRUPTED;
         progressListener.onProgressUpdate(50);
 
         logger.log(context.getString(R.string.auth_auth_form));
@@ -123,16 +123,16 @@ public class MosMetro extends Authenticator {
                     context.getString(R.string.error),
                     context.getString(R.string.auth_error_server)
             ));
-            return STATUS_ERROR;
+            return RESULT.ERROR;
         }
 
-        if (stopped) return STATUS_INTERRUPTED;
+        if (stopped) return RESULT.INTERRUPTED;
         progressListener.onProgressUpdate(75);
 
         logger.log(context.getString(R.string.auth_checking_connection));
         boolean status = false;
         switch (version) {
-            case 1: status = (isConnected() == CHECK_CONNECTED); break;
+            case 1: status = (isConnected() == CHECK.CONNECTED); break;
             case 2:
                 try {
                     JSONObject response = (JSONObject)new JSONParser().parse(client.getPage());
@@ -149,23 +149,23 @@ public class MosMetro extends Authenticator {
                     context.getString(R.string.error),
                     context.getString(R.string.auth_error_connection)
             ));
-            return STATUS_ERROR;
+            return RESULT.ERROR;
         }
 
         progressListener.onProgressUpdate(100);
 
-        return STATUS_CONNECTED;
+        return RESULT.CONNECTED;
     }
     
     @Override
-    public int isConnected() {
+    public CHECK isConnected() {
         Client client = new OkHttp().followRedirects(false);
         try {
             client.get("http://wi-fi.ru", null, pref_retry_count);
         } catch (Exception ex) {
             // Server not responding => wrong network
             logger.log(Logger.LEVEL.DEBUG, ex);
-            return CHECK_WRONG_NETWORK;
+            return CHECK.WRONG_NETWORK;
         }
 
         try {
@@ -175,13 +175,13 @@ public class MosMetro extends Authenticator {
         } catch (Exception ex) {
             // Redirect not found => connected
             logger.log(Logger.LEVEL.DEBUG, ex);
-            return CHECK_CONNECTED;
+            return CHECK.CONNECTED;
         }
 
         if (redirect.contains("login.wi-fi.ru")) // Fallback to the first version
             version = 1;
 
         // Redirect found => not connected
-        return CHECK_NOT_CONNECTED;
+        return CHECK.NOT_CONNECTED;
     }
 }

commit fe252eafa2eac1e11f36f3b9063432c5e706374c

ConnectionService: Fix wrong boolean operation (fixes #65)
This issue was introduced in commit 4003abef7b5ab10b7b194e170ceb8edb1a70967e.

src/pw/thedrhax/mosmetro/services/ConnectionService.java
@@ -327,7 +327,7 @@ public class ConnectionService extends IntentService {
         // Notify user if still connected to Wi-Fi
         if (isWifiConnected()) notify(result);
 
-        if (from_shortcut || (result == Authenticator.RESULT.ALREADY_CONNECTED
+        if (from_shortcut || !(result == Authenticator.RESULT.ALREADY_CONNECTED
                 || result == Authenticator.RESULT.CONNECTED)) return;
 
         // Wait while internet connection is available