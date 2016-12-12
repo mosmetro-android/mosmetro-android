d308b270def79f7c95a02daf454dba1306dbb176
Дмитрий Карих <the.dr.hax@gmail.com>
2016-12-12 13:08:49 +0300

    Settings: Add pref_wifi_bind to control AndroidHacks (fixes #81)

--- src/pw/thedrhax/util/AndroidHacks.java
+++ src/pw/thedrhax/util/AndroidHacks.java
@@ -1,10 +1,12 @@
 package pw.thedrhax.util;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Build;
+import android.preference.PreferenceManager;
 
 public class AndroidHacks {
 
@@ -12,6 +14,10 @@ public class AndroidHacks {
     // Refactored answer from Stack Overflow: http://stackoverflow.com/a/28664841
     public static void bindToWiFi(Context context) {
         if (Build.VERSION.SDK_INT < 21)
+            return;
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        if (!settings.getBoolean("pref_wifi_bind", false))
             return;
 
         ConnectivityManager cm = (ConnectivityManager)
