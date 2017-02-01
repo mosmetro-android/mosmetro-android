mosmetro-android/mosmetro-android

commit e139cdaf1c15db0188d0a247bb87064c77732064

MosMetro v2: Use Activity instead of Dialog for CAPTCHA

src/pw/thedrhax/mosmetro/activities/CaptchaActivity.java
@@ -1,0 +1,116 @@
+/**
+ * Wi-Fi в метро (pw.thedrhax.mosmetro, Moscow Wi-Fi autologin)
+ * Copyright © 2015 Dmitry Karikh <the.dr.hax@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pw.thedrhax.mosmetro.activities;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import pw.thedrhax.mosmetro.R;
+import pw.thedrhax.mosmetro.httpclient.clients.OkHttp;
+import pw.thedrhax.util.Util;
+
+public class CaptchaActivity extends Activity {
+    public static final Object result_sync = new Object();
+    public static String result = null;
+
+    public static String getResult() {
+        synchronized (result_sync) {
+            return result;
+        }
+    }
+
+    public static void setResult(String value) {
+        synchronized (result_sync) {
+            result = value;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.captcha_dialog);
+
+        final int pref_retry_count = Util.getIntPreference(this, "pref_retry_count", 3);
+
+        final Button submit_button = (Button) findViewById(R.id.submit_button);
+
+        final EditText text_captcha = (EditText) findViewById(R.id.text_captcha);
+        text_captcha.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    submit_button.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        final ImageView image_captcha = (ImageView) findViewById(R.id.image_captcha);
+        image_captcha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AsyncTask<Void,Void,Bitmap>() {
+                    @Override
+                    protected Bitmap doInBackground(Void... voids) {
+                        try {
+                            return BitmapFactory.decodeStream(
+                                    new OkHttp().getInputStream(
+                                            getIntent().getStringExtra("url"),
+                                            pref_retry_count
+                                    )
+                            );
+                        } catch (Exception ignored) {}
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Bitmap bitmap) {
+                        if (bitmap != null) image_captcha.setImageBitmap(bitmap);
+                    }
+                }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            }
+        });
+        image_captcha.performClick();
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(text_captcha.getText().toString());
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (getResult() == null) setResult("");
+    }
+}

src/pw/thedrhax/mosmetro/authenticator/providers/MosMetroV2.java
@@ -18,23 +18,11 @@
 
 package pw.thedrhax.mosmetro.authenticator.providers;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.util.Base64;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -45,6 +33,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import pw.thedrhax.mosmetro.R;
+import pw.thedrhax.mosmetro.activities.CaptchaActivity;
 import pw.thedrhax.mosmetro.authenticator.Provider;
 import pw.thedrhax.mosmetro.authenticator.Task;
 import pw.thedrhax.mosmetro.httpclient.Client;
@@ -200,8 +189,19 @@ public class MosMetroV2 extends Provider {
                     }
 
                     // Asking user to enter the code
-                    String code = new CaptchaRunnable(captcha_url).getResult();
-                    if (code.isEmpty()) {
+                    context.startActivity(
+                            new Intent(context, CaptchaActivity.class).putExtra("url", captcha_url)
+                    );
+
+                    // Wait for answer
+                    CaptchaActivity.setResult(null);
+                    while (CaptchaActivity.getResult() == null && !stopped) {
+                        SystemClock.sleep(100);
+                    }
+
+                    // Check the answer
+                    String code = CaptchaActivity.getResult();
+                    if (code == null || code.isEmpty()) {
                         logger.log(String.format(
                                 context.getString(R.string.error),
                                 context.getString(R.string.auth_error_captcha))
@@ -335,121 +335,6 @@ public class MosMetroV2 extends Provider {
             return redirect.contains(".wi-fi.ru") && !redirect.contains("login.wi-fi.ru");
         } catch (Exception ex) {
             return false;
-        }
-    }
-
-    /**
-     * The CaptchaRunnable class is used to prompt user to enter the CAPTCHA.
-     * This operation must be executed on the UI thread and only on the Activity Context.
-     */
-    private class CaptchaRunnable implements Runnable {
-        /**
-         * Thread lock marker.
-         */
-        private boolean locked;
-
-        /**
-         * Result value of the CAPTCHA.
-         */
-        private String result = "";
-
-        /**
-         * URL of the CAPTCHA to solve.
-         */
-        private String url;
-
-        /**
-         * Lock current thread and wait for user to solve the CAPTCHA.
-         * @return Solved CAPTCHA value.
-         */
-        public String getResult() {
-            // Dialog can be created only on the Activity context
-            if (context instanceof Activity) {
-                locked = true;
-
-                ((Activity)context).runOnUiThread(this);
-
-                while (locked && !stopped) {
-                    SystemClock.sleep(100);
-                }
-            }
-
-            return result;
-        }
-
-        /**
-         * Main constructor
-         * @param url   URL of the CAPTCHA to solve
-         */
-        public CaptchaRunnable (String url) {
-            this.url = url;
-        }
-
-        @Override
-        public void run() {
-            final Dialog dialog = new Dialog(context);
-            dialog.setTitle(R.string.auth_captcha_dialog);
-            dialog.setContentView(R.layout.captcha_dialog);
-
-            final Button submit_button = (Button) dialog.findViewById(R.id.submit_button);
-
-            final EditText text_captcha = (EditText) dialog.findViewById(R.id.text_captcha);
-            text_captcha.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                @Override
-                public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                    if (i == EditorInfo.IME_ACTION_DONE) {
-                        submit_button.performClick();
-                        return true;
-                    }
-                    return false;
-                }
-            });
-
-            final ImageView image_captcha = (ImageView) dialog.findViewById(R.id.image_captcha);
-            image_captcha.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    new AsyncTask<Void,Void,Bitmap>() {
-                        @Override
-                        protected Bitmap doInBackground(Void... voids) {
-                            try {
-                                return BitmapFactory.decodeStream(
-                                        client.getInputStream(url, pref_retry_count)
-                                );
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
-                                logger.log(Logger.LEVEL.DEBUG, ex);
-                            }
-                            return null;
-                        }
-
-                        @Override
-                        protected void onPostExecute(Bitmap bitmap) {
-                            if (bitmap != null) image_captcha.setImageBitmap(bitmap);
-                        }
-                    }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-                }
-            });
-            image_captcha.performClick();
-
-            submit_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    result = text_captcha.getText().toString();
-                    locked = false;
-                    dialog.hide();
-                }
-            });
-
-            dialog.setCanceledOnTouchOutside(true);
-            dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialogInterface) {
-                    locked = false;
-                }
-            });
-
-            dialog.show();
         }
     }
 }

AndroidManifest.xml
@@ -74,6 +74,17 @@
             </intent-filter>
         </activity>
 
+        <activity
+            android:name=".activities.CaptchaActivity"
+            android:label="@string/auth_captcha_dialog"
+            android:theme="@style/AppBaseTheme.Dialog"
+            android:launchMode="singleInstance">
+            <intent-filter>
+                <action android:name="pw.thedrhax.mosmetro.activities.ShortcutActivity"/>
+                <category android:name="android.intent.category.DEFAULT"/>
+            </intent-filter>
+        </activity>
+
         <service android:name=".services.ConnectionService"/>
 
         <receiver android:name=".services.NetworkReceiver">

commit ea66aa19444885dccc5df6a2853596c724803e34

CaptchaActivity: Retrieve required cookies from MosMetroV2
Fixes entering the CAPTCHA broken in e139cdaf1c15db0188d0a247bb87064c77732064.

src/pw/thedrhax/mosmetro/activities/CaptchaActivity.java
@@ -32,6 +32,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import pw.thedrhax.mosmetro.R;
+import pw.thedrhax.mosmetro.httpclient.Client;
 import pw.thedrhax.mosmetro.httpclient.clients.OkHttp;
 import pw.thedrhax.util.Util;
 
@@ -56,10 +57,11 @@ public class CaptchaActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.captcha_dialog);
 
+        final String url = getIntent().getStringExtra("url");
+        final Client client = new OkHttp().setCookie(url, "aid", getIntent().getStringExtra("aid"));
         final int pref_retry_count = Util.getIntPreference(this, "pref_retry_count", 3);
 
         final Button submit_button = (Button) findViewById(R.id.submit_button);
-
         final EditText text_captcha = (EditText) findViewById(R.id.text_captcha);
         text_captcha.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -81,10 +83,7 @@ public class CaptchaActivity extends Activity {
                     protected Bitmap doInBackground(Void... voids) {
                         try {
                             return BitmapFactory.decodeStream(
-                                    new OkHttp().getInputStream(
-                                            getIntent().getStringExtra("url"),
-                                            pref_retry_count
-                                    )
+                                    client.getInputStream(url, pref_retry_count)
                             );
                         } catch (Exception ignored) {}
                         return null;

src/pw/thedrhax/mosmetro/authenticator/providers/MosMetroV2.java
@@ -228,6 +228,7 @@ public class MosMetroV2 extends Provider {
                         new Intent(context, CaptchaActivity.class)
                                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 .putExtra("url", captcha_url)
+                                .putExtra("aid", client.getCookies(redirect).get("aid"))
                 );
 
                 // Wait for answer