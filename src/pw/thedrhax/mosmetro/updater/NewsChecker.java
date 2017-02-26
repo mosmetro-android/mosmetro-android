mosmetro-android/mosmetro-android

commit b4195c463ba41f73108fbe8ad166cedd940f9acb

Add class to get app version info

src/pw/thedrhax/mosmetro/updater/NewsChecker.java
@@ -3,8 +3,6 @@ package pw.thedrhax.mosmetro.updater;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.preference.PreferenceManager;
@@ -17,6 +15,7 @@ import pw.thedrhax.mosmetro.R;
 import pw.thedrhax.mosmetro.httpclient.Client;
 import pw.thedrhax.mosmetro.httpclient.clients.OkHttp;
 import pw.thedrhax.util.Notification;
+import pw.thedrhax.util.Version;
 
 public class NewsChecker {
     private Context context;
@@ -66,16 +65,8 @@ public class NewsChecker {
             return false;
         }
 
-        try {
-            PackageInfo pInfo = context
-                    .getPackageManager()
-                    .getPackageInfo(context.getPackageName(), 0);
-
-            if (max_version < pInfo.versionCode)
-                return false;
-        } catch (PackageManager.NameNotFoundException ex) {
+        if (max_version < new Version(context).getVersionCode())
             return false;
-        }
 
         if (settings.getLong("pref_notify_news_id", 0) >= id)
             return false;

src/pw/thedrhax/mosmetro/activities/DebugActivity.java
@@ -2,8 +2,6 @@ package pw.thedrhax.mosmetro.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,6 +15,7 @@ import pw.thedrhax.mosmetro.authenticator.Authenticator;
 import pw.thedrhax.mosmetro.authenticator.Chooser;
 import pw.thedrhax.mosmetro.services.ConnectionService;
 import pw.thedrhax.util.Logger;
+import pw.thedrhax.util.Version;
 
 public class DebugActivity extends Activity {
     // UI Elements
@@ -92,16 +91,10 @@ public class DebugActivity extends Activity {
             case R.id.action_share:
                 Intent send_email = new Intent(Intent.ACTION_SEND);
 
-                String version = "unknown";
-                try {
-                    PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-                    version = pInfo.versionName + "-" + pInfo.versionCode;
-                } catch (PackageManager.NameNotFoundException ignored) {}
-
                 send_email.setType("text/plain");
                 send_email.putExtra(Intent.EXTRA_EMAIL, new String[] {getString(R.string.report_email_address)});
                 send_email.putExtra(Intent.EXTRA_SUBJECT, String.format(
-                        getString(R.string.report_email_subject), version
+                        getString(R.string.report_email_subject), new Version(this).getFormattedVersion()
                 ));
                 send_email.putExtra(Intent.EXTRA_TEXT, logger.get(Logger.LEVEL.DEBUG));
 

src/pw/thedrhax/mosmetro/activities/SettingsActivity.java
@@ -7,8 +7,6 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -22,6 +20,7 @@ import android.widget.Toast;
 
 import pw.thedrhax.mosmetro.R;
 import pw.thedrhax.mosmetro.updater.UpdateCheckTask;
+import pw.thedrhax.util.Version;
 
 import java.util.List;
 
@@ -100,15 +99,10 @@ public class SettingsActivity extends Activity {
 
         // Add version name and code
         Preference app_name = settings.findPreference("app_name");
-        try {
-            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            app_name.setSummary(String.format(
-                    getString(R.string.version),
-                    pInfo.versionName + "-" + pInfo.versionCode)
-            );
-        } catch (PackageManager.NameNotFoundException ex) {
-            app_name.setSummary("");
-        }
+        app_name.setSummary(String.format(
+                getString(R.string.version),
+                new Version(this).getFormattedVersion())
+        );
 
         /*
             Update checking

src/pw/thedrhax/mosmetro/authenticator/Authenticator.java
@@ -2,8 +2,6 @@ package pw.thedrhax.mosmetro.authenticator;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
 import pw.thedrhax.mosmetro.R;
 import pw.thedrhax.mosmetro.updater.URLs;
@@ -14,6 +12,7 @@ import pw.thedrhax.mosmetro.httpclient.Client;
 import pw.thedrhax.mosmetro.httpclient.clients.OkHttp;
 import pw.thedrhax.mosmetro.updater.NewsChecker;
 import pw.thedrhax.util.Logger;
+import pw.thedrhax.util.Version;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -57,8 +56,9 @@ public abstract class Authenticator {
     public RESULT start() {
         stopped = false;
 
-        logger.log(Logger.LEVEL.DEBUG,
-                   String.format(context.getString(R.string.version), getVersion()));
+        logger.log(Logger.LEVEL.DEBUG, String.format(
+                context.getString(R.string.version), new Version(context).getFormattedVersion()
+        ));
         RESULT result = connect();
 
         if (stopped) return result;
@@ -112,22 +112,12 @@ public abstract class Authenticator {
      * System info
      */
 
-    private String getVersion() {
-        PackageInfo pInfo;
-        try {
-            pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            return pInfo.versionName + "-" + pInfo.versionCode;
-        } catch (PackageManager.NameNotFoundException ex) {
-            return "";
-        }
-    }
-
     private void submit_info (RESULT result) {
         String STATISTICS_URL = new CachedRetriever(context)
                 .get(URLs.STAT_URL_SRC, URLs.STAT_URL_DEF) + URLs.STAT_REL_CHECK;
 
         Map<String,String> params = new HashMap<String, String>();
-        params.put("version", getVersion());
+        params.put("version", new Version(context).getFormattedVersion());
         params.put("connected", result == RESULT.CONNECTED ? "1" : "0");
         params.put("ssid", getSSID());
 

src/pw/thedrhax/mosmetro/updater/UpdateCheckTask.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
@@ -15,6 +13,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import pw.thedrhax.mosmetro.R;
 import pw.thedrhax.mosmetro.httpclient.CachedRetriever;
+import pw.thedrhax.util.Version;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -171,18 +170,8 @@ public class UpdateCheckTask extends AsyncTask<Boolean,Void,Void> {
         }
 
         private int getVersion() {
-            if (by_build) {
-                return settings.getInt("pref_updater_build", 0);
-            } else {
-                try {
-                    PackageInfo pInfo = context
-                            .getPackageManager().getPackageInfo(context.getPackageName(), 0);
-
-                    return pInfo.versionCode;
-                } catch (PackageManager.NameNotFoundException ex) {
-                    return Integer.MAX_VALUE;
-                }
-            }
+            return by_build ?
+                    settings.getInt("pref_updater_build", 0) : new Version(context).getVersionCode();
         }
 
         public boolean hasUpdate() {

src/pw/thedrhax/util/Version.java
@@ -1,0 +1,29 @@
+package pw.thedrhax.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+public class Version {
+    private PackageInfo pInfo = null;
+
+    public Version (Context context) {
+        try {
+            pInfo = context
+                    .getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException ignored) {}
+    }
+
+    public String getVersionName() {
+        return (pInfo != null) ? pInfo.versionName : "unknown";
+    }
+
+    public int getVersionCode() {
+        return (pInfo != null) ? pInfo.versionCode : Integer.MAX_VALUE;
+    }
+
+    public String getFormattedVersion() {
+        return getVersionName() + "-" + getVersionCode();
+    }
+}

commit 984928fb1650c83a24dda346ea6ea11a76410865

NewsChecker: Fix version parsing
Broken since b4195c463ba41f73108fbe8ad166cedd940f9acb

src/pw/thedrhax/mosmetro/updater/NewsChecker.java
@@ -62,8 +62,8 @@ public class NewsChecker {
         long id, max_version;
         String title, message, url;
         try {
-            id = Integer.parseInt((String)data.get("id"));
-            max_version = Integer.parseInt((String)data.get("max_version"));
+            id = (Long)data.get("id");
+            max_version = (Long)data.get("max_version");
             title = (String)data.get("title");
             message = (String)data.get("message");
             url = (String)data.get("url");