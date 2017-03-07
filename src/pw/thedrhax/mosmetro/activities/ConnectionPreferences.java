496cc0be0c36db84c74ddad92586187ad2f42c42
Дмитрий Карих <the.dr.hax@gmail.com>
2017-03-07 21:24:09 +0300

    Extract connection preferences to a separate Activity

---
+++ src/pw/thedrhax/mosmetro/activities/ConnectionPreferences.java
@@ -0,0 +1,49 @@
+/**
+ * Wi-Fi в метро (pw.thedrhax.mosmetro, Moscow Wi-Fi autologin)
+ * Copyright © 2015 Dmitry Karikh <the.dr.hax@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pw.thedrhax.mosmetro.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import pw.thedrhax.mosmetro.R;
+
+public class ConnectionPreferences extends Activity {
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_conn);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Populate preferences
+        PreferenceFragment fragment = new SettingsFragment();
+        getFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, fragment)
+                .commit();
+    }
+
+}
