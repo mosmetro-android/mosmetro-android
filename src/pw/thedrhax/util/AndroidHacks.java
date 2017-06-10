2fb1d27d4b826a5b9313fbc0c7e919b0b8698780
Дмитрий Карих <the.dr.hax@gmail.com>
2017-06-10 16:47:40 +0300

    AndroidHacks: Support Wi-Fi binding for API < 21

--- src/pw/thedrhax/util/AndroidHacks.java
+++ src/pw/thedrhax/util/AndroidHacks.java
@@ -26,6 +26,7 @@ import android.net.NetworkInfo;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 
 public final class AndroidHacks {
     private AndroidHacks() {}
@@ -33,9 +34,6 @@ public final class AndroidHacks {
     // Bind current process to a specific network
     // Refactored answer from Stack Overflow: http://stackoverflow.com/a/28664841
     public static void bindToWiFi(@NonNull Context context) {
-        if (Build.VERSION.SDK_INT < 21)
-            return;
-
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         if (!settings.getBoolean("pref_wifi_bind", false))
             return;
@@ -43,17 +41,27 @@ public final class AndroidHacks {
         ConnectivityManager cm = (ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        for (Network network : cm.getAllNetworks()) {
-            NetworkInfo info = cm.getNetworkInfo(network);
-            if (info != null && info.getType() == ConnectivityManager.TYPE_WIFI) {
-                if (Build.VERSION.SDK_INT < 23)
-                    try {
-                        ConnectivityManager.setProcessDefaultNetwork(network);
-                    } catch (IllegalStateException ignored) {}
-                else
-                    cm.bindProcessToNetwork(network);
-                break;
+        if (Build.VERSION.SDK_INT < 21) {
+            cm.setNetworkPreference(ConnectivityManager.TYPE_WIFI);
+        } else {
+            for (Network network : cm.getAllNetworks()) {
+                NetworkInfo info = cm.getNetworkInfo(network);
+                if (info != null && info.getType() == ConnectivityManager.TYPE_WIFI) {
+                    bindToNetwork(cm, network);
+                    break;
+                }
             }
+        }
+    }
+
+    @RequiresApi(21)
+    private static void bindToNetwork(ConnectivityManager cm, Network network) {
+        if (Build.VERSION.SDK_INT < 23) {
+            try {
+                ConnectivityManager.setProcessDefaultNetwork(network);
+            } catch (IllegalStateException ignored) {}
+        } else {
+            cm.bindProcessToNetwork(network);
         }
     }
 }
