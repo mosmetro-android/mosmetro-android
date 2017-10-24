ed2cc460eaa8d53ed2ca05194feedb6851e49cce
Дмитрий Карих <the.dr.hax@gmail.com>
2017-10-24 19:18:46 +0300

    Update ban messages
    Probably we will never see the CAPTCHA again.

--- app/src/main/java/pw/thedrhax/mosmetro/authenticator/providers/MosMetroV2.java
+++ app/src/main/java/pw/thedrhax/mosmetro/authenticator/providers/MosMetroV2.java
@@ -215,7 +215,7 @@ public class MosMetroV2 extends Provider {
             }
 
             private boolean bypass_backdoor() throws IOException {
-                Logger.log(context.getString(R.string.auth_captcha_bypass_backdoor));
+                Logger.log(context.getString(R.string.auth_ban_bypass_backdoor));
 
                 Client tmp_client = new OkHttp(context)
                         .setTimeout(1000)
@@ -266,7 +266,7 @@ public class MosMetroV2 extends Provider {
             }
 
             private boolean bypass_mcc(HashMap<String, Object> vars) throws IOException {
-                Logger.log(context.getString(R.string.auth_captcha_bypass_mcc));
+                Logger.log(context.getString(R.string.auth_ban_bypass_mcc));
                 vars.put("segment", "mcc");
                 client.get(redirect + "/auth?segment=" + vars.get("segment"), null, pref_retry_count);
                 Logger.log(Logger.LEVEL.DEBUG, client.getPageContent().outerHtml());
@@ -281,11 +281,11 @@ public class MosMetroV2 extends Provider {
                 form = client.getPageContent().getElementsByTag("form").first();
 
                 if (form == null) { // No CAPTCHA form found => level 2 block
-                    Logger.log(context.getString(R.string.auth_banned));
+                    Logger.log(context.getString(R.string.auth_ban_message));
 
                     try {
                         if (bypass_mcc(vars)) {
-                            Logger.log(context.getString(R.string.auth_captcha_bypass_success));
+                            Logger.log(context.getString(R.string.auth_ban_bypass_success));
                             vars.put("captcha", "mcc");
                             return true;
                         } else {
@@ -293,7 +293,7 @@ public class MosMetroV2 extends Provider {
                         }
                     } catch (Exception ex) { // Exception type doesn't matter here
                         Logger.log(Logger.LEVEL.DEBUG, ex);
-                        Logger.log(context.getString(R.string.auth_captcha_bypass_fail));
+                        Logger.log(context.getString(R.string.auth_ban_bypass_fail));
                     }
 
                     if (settings.getBoolean("pref_captcha_backdoor", true)) {
@@ -302,7 +302,7 @@ public class MosMetroV2 extends Provider {
 
                     try {
                         if (bypass_backdoor()) {
-                            Logger.log(context.getString(R.string.auth_captcha_bypass_success));
+                            Logger.log(context.getString(R.string.auth_ban_bypass_success));
                             vars.put("result", RESULT.CONNECTED);
                             vars.put("captcha", "backdoor");
                             return false;
@@ -311,7 +311,7 @@ public class MosMetroV2 extends Provider {
                         }
                     } catch (Exception ex) { // Exception type doesn't matter here
                         Logger.log(Logger.LEVEL.DEBUG, ex);
-                        Logger.log(context.getString(R.string.auth_captcha_bypass_fail));
+                        Logger.log(context.getString(R.string.auth_ban_bypass_fail));
                         vars.put("result", RESULT.ERROR);
                         return true;
                     }
