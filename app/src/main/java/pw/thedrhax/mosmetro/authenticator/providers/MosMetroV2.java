19fb86355c23ecf1506f49ba561405a547e1a5da
Дмитрий Карих <the.dr.hax@gmail.com>
2017-12-04 19:20:04 +0300

    Revert "MosMetroV2: Detect midsession in isConnected() (open #135)"
    This reverts commit 1e1e7049f6045b136d259729869b4e8ed622b152.

--- app/src/main/java/pw/thedrhax/mosmetro/authenticator/providers/MosMetroV2.java
+++ app/src/main/java/pw/thedrhax/mosmetro/authenticator/providers/MosMetroV2.java
@@ -461,7 +461,7 @@ public class MosMetroV2 extends Provider {
                 try {
                     JSONObject response = (JSONObject)new JSONParser().parse(client.getPage());
 
-                    if ((Boolean)response.get("result") && isConnected()) {
+                    if ((Boolean)response.get("result")) {
                         Logger.log(context.getString(R.string.auth_connected));
                         vars.put("result", RESULT.CONNECTED);
                         return true;
@@ -483,23 +483,15 @@ public class MosMetroV2 extends Provider {
     public boolean isConnected() {
         Client client = new OkHttp(context).followRedirects(false);
         try {
-            client.get("http://mosgortrans.ru", null, pref_retry_count);
+            client.get("http://wi-fi.ru", null, pref_retry_count);
         } catch (IOException ex) {
             Logger.log(Logger.LEVEL.DEBUG, ex);
             return false;
         }
 
-        // Detect midsession
-        try {
-            String location = client.get300Redirect();
-            if (location.contains("midsession")) {
-                Logger.log(Logger.LEVEL.DEBUG, "Detected midsession: " + location);
-                return true;
-            }
-        } catch (ParseException ignored) {}
-
         try {
             redirect = client.parseMetaRedirect();
+            Logger.log(Logger.LEVEL.DEBUG, client.getPageContent().outerHtml());
             Logger.log(Logger.LEVEL.DEBUG, redirect);
         } catch (ParseException ex) {
             // Redirect not found => connected
