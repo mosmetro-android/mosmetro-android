dd472f5aa3f57e59ae44e5f6236140aa9d625c3c
Дмитрий Карих <the.dr.hax@gmail.com>
2017-12-25 23:09:24 +0300

    Settings: Set default delays to 20-40 seconds

--- app/src/main/java/pw/thedrhax/util/Randomizer.java
+++ app/src/main/java/pw/thedrhax/util/Randomizer.java
@@ -39,8 +39,8 @@ public class Randomizer {
     public Randomizer(Context context) {
         this.context = context;
 
-        delay_min = Util.getIntPreference(context, "pref_delay_range_min", 5);
-        delay_max = Util.getIntPreference(context, "pref_delay_range_max", 10);
+        delay_min = Util.getIntPreference(context, "pref_delay_range_min", 20);
+        delay_max = Util.getIntPreference(context, "pref_delay_range_max", 40);
         if (delay_min > delay_max) { // User tries to break everything again...
             int temp = delay_max;
             delay_max = delay_min;
