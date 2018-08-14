62bf95d3612442d1494bd9d8dc38992e451ae1a1
Дмитрий Карих <the.dr.hax@gmail.com>
2018-08-14 18:42:37 +0300

    Add ACRA integration to collect crash reports without Google Play

---
+++ app/src/main/java/pw/thedrhax/mosmetro/MosMetroApp.java
@@ -0,0 +1,23 @@
+package pw.thedrhax.mosmetro;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraHttpSender;
+import org.acra.data.StringFormat;
+import org.acra.sender.HttpSender;
+
+@AcraCore(buildConfigClass = BuildConfig.class,
+          reportFormat = StringFormat.JSON)
+@AcraHttpSender(uri = BuildConfig.API_URL_DEFAULT + BuildConfig.API_REL_ACRA,
+                httpMethod = HttpSender.Method.POST)
+public class MosMetroApp extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.init(this);
+    }
+}
