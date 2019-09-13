c9e04bdca434a93ea38baabb76be099c57b11bb7
Дмитрий Карих <the.dr.hax@gmail.com>
2019-09-13 20:18:42 +0300

    ACRA: Migrate to Tracepot
    HockeyApp will be closed in next two months in favor of AppCenter that appears to not be compatible with ACRA anymore.

---
+++ app/src/main/java/pw/thedrhax/mosmetro/acra/CustomHttpSender.java
@@ -0,0 +1,48 @@
+package pw.thedrhax.mosmetro.acra;
+
+import org.acra.ReportField;
+import org.acra.config.CoreConfiguration;
+import org.acra.data.CrashReportData;
+import org.acra.data.StringFormat;
+import org.acra.sender.HttpSender;
+import org.acra.sender.ReportSenderException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+class CustomHttpSender extends HttpSender {
+    public CustomHttpSender(CoreConfiguration config, Method method, StringFormat type, String formUri) {
+        super(config, method, type, formUri);
+    }
+
+    public CustomHttpSender(CoreConfiguration config, Method method, StringFormat type) {
+        super(config, method, type);
+    }
+
+    @Override
+    public void send(Context context, CrashReportData report) throws ReportSenderException {
+        JSONObject build_config = (JSONObject) report.get(ReportField.BUILD_CONFIG.toString());
+
+        try {
+            if (build_config != null) {
+                Object branch = build_config.get("BRANCH_NAME");
+                Object build = build_config.get("BUILD_NUMBER");
+
+                if (branch != null && build != null) {
+                    report.put(ReportField.APP_VERSION_NAME, branch.toString() + " #" + build.toString());
+                    report.put(ReportField.APP_VERSION_CODE, Integer.parseInt(build.toString()));
+                }
+            }
+        } catch (JSONException|NumberFormatException ignored) {}
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        if (!settings.getBoolean("pref_debug_last_log", true)) {
+            report.put(ReportField.APPLICATION_LOG, (String) null);
+        }
+
+        super.send(context, report);
+    }
+}
