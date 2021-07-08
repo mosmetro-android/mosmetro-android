02d0db6b7c7007923277e71b05d889b26bae6e09
Дмитрий Карих <the.dr.hax@gmail.com>
2021-07-08 20:54:34 +0300

    Provider: Remove Enforta stub

--- app/src/main/java/pw/thedrhax/mosmetro/authenticator/Provider.java
+++ app/src/main/java/pw/thedrhax/mosmetro/authenticator/Provider.java
@@ -28,7 +28,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import pw.thedrhax.mosmetro.R;
-import pw.thedrhax.mosmetro.authenticator.providers.Enforta;
 import pw.thedrhax.mosmetro.authenticator.providers.MAInet;
 import pw.thedrhax.mosmetro.authenticator.providers.MosMetroV1;
 import pw.thedrhax.mosmetro.authenticator.providers.MosMetroV2;
@@ -95,7 +94,6 @@ public abstract class Provider extends LinkedList<Task> {
         else if (MosMetroV2mcc.match(response)) return new MosMetroV2mcc(context, response);
         else if (MosMetroV1.match(response)) return new MosMetroV1(context, response);
         else if (MAInet.match(response) && settings.getBoolean("pref_mainet", true)) return new MAInet(context, response);
-        else if (Enforta.match(response)) return new Enforta(context);
         else return new Unknown(context, response);
     }
 
